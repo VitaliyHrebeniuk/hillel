5e3c9ef4b0d0251a456679992bb8b40c71de9b57
vitaliihrebeniuk <vitaliy.hrebeniuk@gmail.com>
2021-11-15 08:32:18 +0200

    Home Work 4

--- src/Home_Works/HomeWork4/arrayMinToMax.java
+++ src/Home_Works/HomeWork4/arrayMinToMax.java
@@ -1,15 +1,26 @@
 package Home_Works.HomeWork4;
 
+import java.util.Arrays;
+
 public class arrayMinToMax {
     public static void main(String[] args) {
-        int[] arr = {5123123,412,0,241231,1,2};
-        int[] arrSorted;
-        for(int i=0;i<arr.length;i++){
-            for(int k=0;k<arr.length; k++){
-                if(arr[k]<arr[arr.length - 1 - i]) {
-                    System.out.println(arr[k] + " " + arr[i]);
+        int[] arr = {5,3,2,1,9,6,7,8};
+        for (int i = 0; i < arr.length; i++) {
+            int min = arr[i];
+            int min_i = i;
+            for (int j = i+1; j < arr.length; j++) {
+                if (arr[j] < min) {
+                    min = arr[j];
+                    min_i = j;
                 }
             }
+            if (i != min_i) {
+                int tmp = arr[i];
+                arr[i] = arr[min_i];
+                arr[min_i] = tmp;
+            }
         }
+        System.out.println(Arrays.toString(arr));
     }
 }
+
--- src/Home_Works/HomeWork4/charStringReverse.java
+++ src/Home_Works/HomeWork4/charStringReverse.java
@@ -3,9 +3,11 @@ package Home_Works.HomeWork4;
 public class charStringReverse {
     public static void main(String[] args) {
         String word = "vitalik";
-        char wordResult;
-        for(int i=0;i<word.length();i++){
-
+        char[] array = word.toCharArray();
+        String result = "";
+        for (int i = array.length - 1; i >= 0; i = i - 1) {
+            result = result + array[i];
+        }
+        System.out.println(result);
         }
     }
-}
