7af6262e09fc904ba43c748e2cca854ab08634fe
vitaliihrebeniuk <vitaliy.hrebeniuk@gmail.com>
2021-11-24 16:20:50 +0200

    дополнил ДЗ 7

--- src/Home_Works/HomeWork7/App.java
+++ src/Home_Works/HomeWork7/App.java
@@ -1,13 +1,8 @@
 package Home_Works.HomeWork7;
-import static Home_Works.HomeWork7.FractionNumbers.*;
 
 public class App {
     public static void main(String[] args) {
-        FractionNumbers fractionNumbers = new FractionNumbers(first_number,second_number,method);
-        first_number = fractionNumbers.setFirst_number();
-        second_number = fractionNumbers.setSecond_number();
-        method = fractionNumbers.setMethod();
+        FractionNumbers fractionNumbers = new FractionNumbers();
         System.out.println(fractionNumbers.toString());
     }
-
 }
--- src/Home_Works/HomeWork7/FractionNumbers.java
+++ src/Home_Works/HomeWork7/FractionNumbers.java
@@ -2,57 +2,21 @@ package Home_Works.HomeWork7;
 
 import java.util.Scanner;
 
+
 public class FractionNumbers {
-    static int first_number;
-    static int second_number;
-    static String method;
+    private int first_number;
+    private int second_number;
 
-    public FractionNumbers(int first_number, int second_number, String method) {
-        this.first_number = FractionNumbers.first_number;
-        this.second_number = FractionNumbers.second_number;
-        this.method = FractionNumbers.method;
-    }
-
-    @Override
-    public String toString(){
-        if(method.equals("plus")) {
-            return "" + plus(first_number, second_number);
-        }else if(method.equals("minus")){
-            return "" + minus(first_number, second_number);
-        }else if(method.equals("multiply")){
-            return "" + multiply(first_number, second_number);
-        }else if(method.equals("divide")){
-            return "" + divide(first_number, second_number);
-        }else{
-            return "Wrong method!";
-        }
-    }
-
-    public int setSecond_number() {
-        FractionNumbers.first_number = getIntFromScanner1();
-        return first_number;
-    }
-
-    public int setFirst_number() {
-        FractionNumbers.second_number = getIntFromScanner();
-        return second_number;
-    }
-
-    public String setMethod() {
-        FractionNumbers.method = getStringFromScanner();
-        return method;
-    }
-
-    public int getIntFromScanner() {
+    public void getIntFromScanner() {
         System.out.println("Write first_number please:");
         Scanner scanner = new Scanner(System.in);
-        return scanner.nextInt();
+        first_number = scanner.nextInt();
     }
 
-    public int getIntFromScanner1() {
+    public void getIntFromScanner1() {
         System.out.println("Write second_number please:");
         Scanner scanner = new Scanner(System.in);
-        return scanner.nextInt();
+        second_number = scanner.nextInt();
     }
 
     public String getStringFromScanner() {
@@ -61,7 +25,29 @@ public class FractionNumbers {
         return scanner.nextLine();
     }
 
-    public int plus(int first_number, int second_number){return first_number + second_number;}
+    @Override
+    public String toString(){
+        //вызываем для получения первого числа
+        getIntFromScanner();
+        //вызываем для получения второго числа
+        getIntFromScanner1();
+        //спрашиваем какой метод нам нужен
+        String method = getStringFromScanner();
+        switch(method) {
+            case "plus":
+                return "" + plus(first_number, second_number);
+            case"minus":
+                return "" + minus(first_number, second_number);
+            case"multiply":
+                return "" + multiply(first_number, second_number);
+            case"divide":
+                return "" + divide(first_number, second_number);
+            default:
+                return "Wrong method!";
+        }
+    }
+
+    public int plus(int firstNumber, int second_number){return first_number + second_number;}
     public int minus(int first_number, int second_number){return first_number - second_number;}
     public int multiply(int first_number, int second_number){return first_number * second_number;}
     public int divide(int first_number, int second_number){return first_number / second_number;}
